06688efd66adbbdf17e237c9563f36c8dc479fbe
psmniranjana <psmniranjana@students.nsbm.ac.lk>
2025-03-31 13:28:16 +0530

    6th

--- forms/contact.php
+++ forms/contact.php
@@ -1,13 +1,13 @@
 <?php
   /**
-  * Requires the "PHP Email Form" library
-  * The "PHP Email Form" library is available only in the pro version of the template
-  * The library should be uploaded to: vendor/php-email-form/php-email-form.php
-  * For more info and help: https://bootstrapmade.com/php-email-form/
-  */
+   * Requires the "PHP Email Form" library
+   * The "PHP Email Form" library is available only in the pro version of the template
+   * The library should be uploaded to: vendor/php-email-form/php-email-form.php
+   * For more info and help: https://bootstrapmade.com/php-email-form/
+   */
 
-  // Replace contact@example.com with your real receiving email address
-  $receiving_email_address = 'contact@example.com';
+  // Replace the email with your real receiving email address
+  $receiving_email_address = 'maleeshaniranjana6@gmail.com'; // Updated email
 
   if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
     include( $php_email_form );
@@ -18,7 +18,7 @@
   $contact = new PHP_Email_Form;
   $contact->ajax = true;
   
-  $contact->to = $receiving_email_address;
+  $contact->to = $receiving_email_address;  // Sending to your email
   $contact->from_name = $_POST['name'];
   $contact->from_email = $_POST['email'];
   $contact->subject = $_POST['subject'];
@@ -33,9 +33,11 @@
   );
   */
 
+  // Add form messages to be sent
   $contact->add_message( $_POST['name'], 'From');
   $contact->add_message( $_POST['email'], 'Email');
   $contact->add_message( $_POST['message'], 'Message', 10);
 
+  // Send the email
   echo $contact->send();
 ?>
